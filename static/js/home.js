52ec0b110e80e2b84445a520d7f0917afa29b20e
csik <robotic@gmail.com>
2022-02-07 12:12:52 -0500

    still trying people

--- static/js/home.js
+++ static/js/home.js
@@ -234,128 +234,125 @@ var projectCards;
     }
 
     function singleColumnRow(gallery, entries, i) {
-
-      let entry0 = document.createElement("div");
-      entry0.classList.add("col-3", "m-0", "p-0");
-      gallery.appendChild(entry0);
-
       let entry1 = document.createElement("div");
-      entry1.classList.add("col-6", "m-0", "p-0");
+      entry1.classList.add("col-12", "m-0", "p-0");
       entry1.appendChild(entries[i].cloneNode(true));
       entry1.children[0].classList.add("img-type-1");
       gallery.appendChild(entry1);
       i++;
     }
-   const xah_randomize_children_f = ((nodeX) => {
-    // nodeX can be any html element
-    // randomize its children
-    // http://xahlee.info/js/js_dom_randomize_list.html
-    // version 2017-05-11
 
-    const newNode = nodeX.cloneNode(true);
-    const xChildren = newNode.children;
-    const newNodeFrag = document.createDocumentFragment();
+    const xah_randomize_children_f = ((nodeX) => {
+      // nodeX can be any html element
+      // randomize its children
+      // http://xahlee.info/js/js_dom_randomize_list.html
+      // version 2017-05-11
+  
+      const newNode = nodeX.cloneNode(true);
+      const xChildren = newNode.children;
+      const newNodeFrag = document.createDocumentFragment();
+  
+      while (xChildren.length > 0) {
+          newNodeFrag.appendChild( xChildren [Math.floor(Math.random() * xChildren.length)] );
+      };
+  
+      nodeX.innerHTML = "";
+      nodeX.appendChild(newNodeFrag);
+    });
+  
+    function showAchievements() {
+      // show achievements from achievements-holder div
+      let gallery = document.getElementById("gallery");
+      if (gallery == null) {
+        return
+      }
+      gallery.innerHTML = "";
+      xah_randomize_children_f(document.getElementById("achievements-holder"));
+      const entries = document.getElementById("achievements-holder").children;
+      let len = entries.length;
+      let i = 0;
+      let rowNumber = 1;
+      while (i < len) {
+        if (isLaptop) {
+          if (i + 4 <= len) {
+            if (rowNumber % 2) {
+              fourColumRow(gallery, entries, i);
+            } else {
+              fourColumnReversedRow(gallery, entries, i);
+            }
+            i += 4;
 
-    while (xChildren.length > 0) {
-        newNodeFrag.appendChild( xChildren [Math.floor(Math.random() * xChildren.length)] );
-    };
-
-    nodeX.innerHTML = "";
-    nodeX.appendChild(newNodeFrag);
-});
-
-function showAchievements() {
-  // show achievements from achievements-holder div
-  let gallery = document.getElementById("gallery");
-  if (gallery == null) {
-    return
-  }
-  gallery.innerHTML = "";
-  xah_randomize_children_f(document.getElementById("achievements-holder"));
-  const entries = document.getElementById("achievements-holder").children;
-  let len = entries.length;
-  let i = 0;
-  let rowNumber = 1;
-  while (i < len) {
-    if (isLaptop) {
-      if (i + 4 <= len) {
-        if (rowNumber % 2) {
-          fourColumRow(gallery, entries, i);
+          } else if (i + 3 <= len) {
+            if (rowNumber % 2) {
+              threeColumnRow(gallery, entries, i);
+            } else {
+              threeColumnReversedRow(gallery, entries, i);
+            }
+            i += 3;
+          } else if (i + 2 <= len) {
+            twoColumnRow(gallery, entries, i);
+            i += 2;
+          } else {
+            singleColumnRow(gallery, entries, i);
+            i++;
+          }
+        } else if (isTablet) {
+          if (i + 2 <= len) {
+            twoColumnRow(gallery, entries, i);
+            i += 2;
+          } else {
+            singleColumnRow(gallery, entries, i);
+            i++;
+          }
         } else {
-          fourColumnReversedRow(gallery, entries, i);
+          singleColumnRow(gallery, entries, i);
+          i++;
         }
-        i += 4;
+        rowNumber++;
+      }
 
-      } else if (i + 3 <= len) {
-        if (rowNumber % 2) {
-          threeColumnRow(gallery, entries, i);
-        } else {
-          threeColumnReversedRow(gallery, entries, i);
+      // show full image on click
+      let elements = document.getElementsByClassName("achievement-entry");
+      len = elements.length;
+      for (let i = 0; i < len; i++) {
+        elements[i].onclick = function () {
+          let achievements = document.getElementsByClassName("achievement-entry");
+          let len2 = achievements.length;
+          for (let j = 0; j < len2; j++) {
+            achievements[j].classList.toggle("hidden");
+          }
+          this.classList.toggle("achievement-details");
+          this.classList.toggle("hidden");
+          this.parentElement.classList.toggle("col-lg-12");
+          this.parentElement.classList.toggle("col-md-12");
+          this.parentElement.classList.toggle("col-sm-12");
+          if (this.children["SmallImage"].hasAttribute("active")) {
+            let mainLogo = this.children["LargeImage"].getAttribute("Style");
+            this.children["LargeImage"].setAttribute("active", true);
+            this.children["SmallImage"].removeAttribute("active");
+
+            this.setAttribute("Style", mainLogo);
+          } else {
+            let mainLogo = this.children["SmallImage"].getAttribute("Style");
+            this.children["SmallImage"].setAttribute("active", true);
+            this.children["LargeImage"].removeAttribute("active");
+            this.setAttribute("Style", mainLogo);
+          }
+
+          if (this.children["caption"] != undefined) {
+            this.children["caption"].classList.toggle("hidden");
+          }
+          if (this.children["enlarge-icon"] != undefined) {
+            this.children["enlarge-icon"].classList.toggle("fa-search-plus");
+            this.children["enlarge-icon"].classList.toggle("fa-times");
+          }
+          if (this.children["achievement-title"] != undefined) {
+            this.children["achievement-title"].classList.toggle("hidden");
+          }
         }
-        i += 3;
-      } else if (i + 2 <= len) {
-        twoColumnRow(gallery, entries, i);
-        i += 2;
-      } else {
-        singleColumnRow(gallery, entries, i);
-        i++;
-      }
-    } else if (isTablet) {
-      if (i + 2 <= len) {
-        twoColumnRow(gallery, entries, i);
-        i += 2;
-      } else {
-        singleColumnRow(gallery, entries, i);
-        i++;
-      }
-    } else {
-      singleColumnRow(gallery, entries, i);
-      i++;
-    }
-    rowNumber++;
-  }
-
-  // show full image on click
-  let elements = document.getElementsByClassName("achievement-entry");
-  len = elements.length;
-  for (let i = 0; i < len; i++) {
-    elements[i].onclick = function () {
-      let achievements = document.getElementsByClassName("achievement-entry");
-      let len2 = achievements.length;
-      for (let j = 0; j < len2; j++) {
-        achievements[j].classList.toggle("hidden");
-      }
-      this.classList.toggle("achievement-details");
-      this.classList.toggle("hidden");
-      this.parentElement.classList.toggle("col-lg-12");
-      this.parentElement.classList.toggle("col-md-12");
-      this.parentElement.classList.toggle("col-sm-12");
-      if (this.children["SmallImage"].hasAttribute("active")) {
-        let mainLogo = this.children["LargeImage"].getAttribute("Style");
-        this.children["LargeImage"].setAttribute("active", true);
-        this.children["SmallImage"].removeAttribute("active");
-
-        this.setAttribute("Style", mainLogo);
-      } else {
-        let mainLogo = this.children["SmallImage"].getAttribute("Style");
-        this.children["SmallImage"].setAttribute("active", true);
-        this.children["LargeImage"].removeAttribute("active");
-        this.setAttribute("Style", mainLogo);
-      }
-
-      if (this.children["caption"] != undefined) {
-        this.children["caption"].classList.toggle("hidden");
-      }
-      if (this.children["enlarge-icon"] != undefined) {
-        this.children["enlarge-icon"].classList.toggle("fa-search-plus");
-        this.children["enlarge-icon"].classList.toggle("fa-times");
-      }
-      if (this.children["achievement-title"] != undefined) {
-        this.children["achievement-title"].classList.toggle("hidden");
       }
     }
-  }
-} showAchievements();
+    showAchievements();
 
     // re-render custom functions on window resize
     window.onresize = function () {
